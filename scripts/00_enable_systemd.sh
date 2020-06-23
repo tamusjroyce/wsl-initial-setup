686c2169c5014012e65c0dcc4031de26069ee301
Relief Melone <relief.melone@gmail.com>
2020-06-23 18:08:25 +0200

    added systemd for rootfs

---
+++ scripts/00_enable_systemd.sh
@@ -0,0 +1,17 @@
+BASEDIR=$(dirname $0)
+
+# Updaete the system
+apt update && apt upgrade -y
+
+# Install the required packages for SystemD
+apt install -yqq fontconfig daemonize
+# Creates a default user and adds it to the sudo group
+useradd -m -s /bin/bash -G sudo mk8s
+# Reset the password of the default user
+passwd mk8s
+
+cp $BASEDIR/../etc/sudoers > /etc/sudoers
+cp $BASEDIR/../etc/wsl.conf /etc/wsl.conf
+cp $BASEDIR/../etc/profile.d/00-wsl2-systemd.sh /etc/profile.d/00-wsl2-systemd.sh
+
+echo "sytemd enabled. Please restart wsl"
