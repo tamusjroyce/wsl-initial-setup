941e290b7b83652f7364638a3b67283afb527a58
Relief Melone <relief.melone@gmail.com>
2020-06-27 00:56:13 +0200

    updated inputrc and bashrc

--- scripts/01.09_install_personal_settings.sh
+++ scripts/01.09_install_personal_settings.sh
@@ -1,16 +1,18 @@
-printf "\n\
+BASEDIR=$(dirname $0)
+
+if [[ $SUDO_USER == "reliefmelone" ]]; then
+
+  printf "\n\
+  User reliefmelone detected
 ==========================\n\
      PERSONAL SETTINGS \n\
 ==========================\n\
 "
-
-BASEDIR=$(dirname $0)
-
-if [[ $SUDO_USER == "reliefmelone" ]]; then
+  echo "=======Setting .gitconfig"
   cp $BASEDIR/../home/.gitconfig /home/$SUDO_USER/.gitconfig
   cp $BASEDIR/../home/.gitconfig ~/.gitconfig
-
+  echo "=======Setting inputrc"
   cp $BASEDIR/../etc/inputrc /etc/inputrc
-
+  echo "=======Linking coding_local"
   ln -s /mnt/c/coding_local /coding_local
 fi
