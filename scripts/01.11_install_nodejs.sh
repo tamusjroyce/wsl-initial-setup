e27fdb823e249b99d5783732605b06a4e2f92f4a
Relief Melone <relief.melone@gmail.com>
2020-08-21 22:22:07 +0200

    use stable

--- scripts/01.11_install_nodejs.sh
+++ scripts/01.11_install_nodejs.sh
@@ -14,3 +14,6 @@ sudo -u $SUDO_USER curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "/home/$SUDO_USER/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install stable
+nvm use stable
