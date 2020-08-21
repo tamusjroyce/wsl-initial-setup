fc4343f0cbe969053ddf1761126bc930e89d5c9a
Relief Melone <relief.melone@gmail.com>
2020-08-21 22:21:02 +0200

    nvm install fixed

--- scripts/01.11_install_nodejs.sh
+++ scripts/01.11_install_nodejs.sh
@@ -10,4 +10,7 @@ if [[ !$(command -v curl) ]]; then
   apt-get install -y curl
 fi
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+sudo -u $SUDO_USER curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | sudo -u $SUDO_USER bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "/home/$SUDO_USER/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
