9214abdb6aa4ad8b11e2a95f9370a382b352b860
root <root@SB2-RM.localdomain>
2020-08-25 01:05:21 +0200

    first test

--- scripts/01.08_install_skaffold.sh
+++ scripts/01.08_install_skaffold.sh
@@ -8,6 +8,4 @@ if [[ !$(command -v curl) ]]
   apt-get install -y curl
 fi
 
-curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
-install skaffold /usr/local/bin/
-rm -rf skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 -o /usr/bin/skaffold
