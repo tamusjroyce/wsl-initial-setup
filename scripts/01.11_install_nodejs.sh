02291bda3fdf01f82b330cd5ff1e5113bc20dabc
Relief Melone <relief.melone@gmail.com>
2020-10-10 17:29:35 +0200

    enabled nodejs to beeing able to install global packages

--- scripts/01.11_install_nodejs.sh
+++ scripts/01.11_install_nodejs.sh
@@ -10,9 +10,10 @@ if [[ !$(command -v curl) ]]; then
   apt-get install -y curl
 fi
 
+curl  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 sudo -u $SUDO_USER curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | sudo -u $SUDO_USER bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "/home/$SUDO_USER/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+export NVM_DIR=/home/$SUDO_USER/.nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install stable
